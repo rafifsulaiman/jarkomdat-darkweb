3dc420a0ea2621bb3bb1539de5ce79aea4ce092b
rafif sulaiman <rafif.sulaiman@ui.ac.id>
2025-12-08 20:28:54 +0700

    initialize project

---
+++ src/main/java/jarkomdat/darkweb/jarkomdat_darkweb/JarkomdatDarkwebApplication.java
@@ -0,0 +1,13 @@
+package jarkomdat.darkweb.jarkomdat_darkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarkomdatDarkwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarkomdatDarkwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jarkomdat/darkweb/jarkomdat_darkweb/JarkomdatDarkwebApplicationTests.java
@@ -0,0 +1,13 @@
+package jarkomdat.darkweb.jarkomdat_darkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarkomdatDarkwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
